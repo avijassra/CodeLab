f80f6670dd3a238508d89fa96ec8933b3f41d78b
Avi Jassra <avijassra@hotmail.com>
2013-01-28 11:37:11 -0500

    updated code

---
+++ src/Games/GamesFoundation/resources/jewwlWarrior-game.js
@@ -0,0 +1,19 @@
+jewel.game = (function() {
+	var dom = jewel.dom,
+	$ = dom.$;
+	// hide the active screen (if any) and show the screen
+	// with the specified id
+	function showScreen(screenId) {
+		var activeScreen = $("#game.screen.active")[0],
+			screen = $("#" + screenId)[0];
+		if (activeScreen) {
+			dom.removeClass(screen, "active");
+		} 
+		
+		dom.addClass(screen, "active");
+	} // expose public methods
+	
+	return {
+		showScreen : showScreen
+	};
+})();
--- src/Games/GamesFoundation/resources/jewwlWarrior-loader.js
+++ src/Games/GamesFoundation/resources/jewwlWarrior-loader.js
@@ -1,3 +1,19 @@
-window.addEventListener('load', function(e) {
-	alert(1);
+var jewel = {};
+// wait until main document is loaded
+window.addEventListener("load", function()
+{
+	// start dynamic loading
+	Modernizr.load([
+	{ 
+		// these files are always loaded
+		load : [
+		"resources/sizzle.js",
+		"resources/sizzle-dom-ext.js",
+		"resources/jewwlWarrior-game.js"
+		],
+		// called when all files have finished	loading and executing
+		complete : function() {
+			jewel.game.showScreen('splash-screen');
+		}
+	} ]);
 }, false);
